ee4a6ef83ae98d6e27718f81492b5858f09b05c4
EFREN NARVAEZ <efrennm86@gmail.com>
2024-01-06 20:02:16 -0600

    Se hace uso de la paquetería express-async-handler al englobar las funciones de los controladores

--- backend/controllers/tareasController.js
+++ backend/controllers/tareasController.js
@@ -1,22 +1,24 @@
- const getTareas = (req, res) => {
+const asyncHandler = require(express-async-hanldler)
+
+const getTareas = (req, res) => {
     res.status(200).json({message:'Obtener algo'})
  }
 
- const setTareas = async (req, res) => {
+ const setTareas =asyncHandler( async (req, res) => {
     if(!req.body.texto){
         res.status(400)
         throw new Error ("Por favor teclea la descripción")
     }
     res.status(201).json({message:'Crear nueva tarea'})
- }
+ })
 
- const modificarTareas = async (req, res) => {
+ const modificarTareas = asyncHandler( async (req, res) => {
     res.status(200).json({message:`Modificar la tarea número ${req.params.id}`})
- }
+ })
 
- const eliminarTareas = async (req, res) => {
+ const eliminarTareas = asyncHandler( async (req, res) => {
     res.status(200).json({message:`Eliminar la tarea número ${req.params.id}`})
- }
+ })
 
 
 module.exports = {
