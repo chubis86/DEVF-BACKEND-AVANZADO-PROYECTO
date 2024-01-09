54d242f9ad57c916a599757f2ecc92b260f36c53
EFREN NARVAEZ <efrennm86@gmail.com>
2024-01-08 19:32:57 -0600

    Se crea el módulo de protect para poder proteger ciertos endpoint

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const { registrarUser, loginUser, datosUser } = require('../controllers/usersController')
 
 
-
+//Público
 router.post('/', registrarUser)
 router.post('/login', loginUser)
+
+//Privado
 router.get('/data', datosUser)
 
 module.exports = router
